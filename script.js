674b11aa61fa7c7e3342adb7cd44d204ac28ae55
Mokete <emtyabekana@gmail.com>
2024-10-11 19:18:43 +0200

    Passed the evnt as a parameter to the annonymous function.

--- script.js
+++ script.js
@@ -6,8 +6,9 @@ document.addEventListener('DOMContentLoaded', function() {
     const form =document.getElementById('registration-form');
     const feedbackDiv=document.getElementById('form-feedback');
 
-    form.addEventListener('submit',function(){
-        event.preventDefault();
-        
+    form.addEventListener('submit',function(event){
+
+        event.preventDefault()
+
     });
 });
