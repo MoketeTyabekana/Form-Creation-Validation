5028202441343aaefc87a816a0513afd910f6cec
Mokete <emtyabekana@gmail.com>
2024-10-12 16:43:18 +0200

    declared a constant apiUrl with an API link.

--- fetch-data.js
+++ fetch-data.js
@@ -2,6 +2,9 @@
 //Initialize the Async Function
 
 async function fetchUserData() {
-    
+
+    //declare a constant apiUrl
+
+    const apiUrl='https://jsonplaceholder.typicode.com/users';
     
 }
