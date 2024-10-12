7f2344c479abd318d6823431032de3a53dbf8831
Mokete <emtyabekana@gmail.com>
2024-10-12 16:49:11 +0200

    created a constant variable to store the fetched data using the await keyword with fetch to psynchronously get data from the API

--- fetch-data.js
+++ fetch-data.js
@@ -10,5 +10,19 @@ async function fetchUserData() {
     //Select the Data Container Element
 
     const dataContainer=document.getElementById('api-data');
+
+    //Fetch Data Using try-catch
+
+    try{
+
+        //In the try block, use the await keyword with the fetch function to asynchronously get data from apiUrl. Store the response in a constant named response.
+
+        const response=await fetch(apiUrl);
+        
+
+    }
+    catch{
+
+    }
     
 }
