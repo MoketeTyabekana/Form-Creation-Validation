c403278c698998d505543c89529770683fa7e35a
Mokete <emtyabekana@gmail.com>
2024-10-12 16:45:21 +0200

    Selected the Data Container Element using get element by ID method.

--- fetch-data.js
+++ fetch-data.js
@@ -6,5 +6,9 @@ async function fetchUserData() {
     //declare a constant apiUrl
 
     const apiUrl='https://jsonplaceholder.typicode.com/users';
+
+    //Select the Data Container Element
+
+    const dataContainer=document.getElementById('api-data');
     
 }
